befbcc18d228817c1e7bac7215e7b76cccb8afad
김유현 <kinyuhyun@gim-yuhyeons-MacBook-Air.local>
2024-03-31 22:54:20 +0900

    remove snap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import "./App.css";
 import { Helmet } from "react-helmet-async";
+
 import fav from "./assets/favicon-16x16.png";
 import favLg from "./assets/favicon-32x32.png";
 import apple from "./assets/apple-touch-icon.png";
 import android from "./assets/android-chrome-192x192.png";
 import androidLg from "./assets/android-chrome-512x512.png";
+
 function App() {
   return (
     <>
-      <Helmet>
+      <Helmet
+        link={[
+          { rel: "icon", href: "./assets/favicon.ico", type: "image/x-icon" },
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: { apple },
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "192x192",
+            href: { android },
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "512x512",
+            href: { androidLg },
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: { favLg },
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: { fav },
+          },
+          { rel: "manifest", href: "./assets/site.webmanifest" },
+        ]}
+      >
         <title>Seo Test...</title>
         <meta property="og:site_name" content="SEO Test" />
         <meta property="og:title" content="SEO Test Page" />
@@ -23,14 +59,6 @@ function App() {
         <meta name="twitter:description" content="SEO Test 입니다" />
         <meta name="twitter:image" content="" />
         <meta name="twitter:card" content="" />
-
-        <link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="180x180" href={apple} />
-        <link rel="icon" type="image/png" sizes="192x192" href={android} />
-        <link rel="icon" type="image/png" sizes="512x512" href={androidLg} />
-        <link rel="icon" type="image/png" sizes="32x32" href={favLg} />
-        <link rel="icon" type="image/png" sizes="16x16" href={fav} />
-        <link rel="manifest" href="./assets/site.webmanifest" />
       </Helmet>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { hydrateRoot } from "react-dom/client"; // Import hydrateRoot from "react-dom/client"
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 
-function Root() {
-  return (
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-}
-
-hydrateRoot(document.getElementById("root"), () => <Root />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
