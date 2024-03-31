675ac00d184a03443ab6996a3bdc2931f992e100
김유현 <kinyuhyun@gim-yuhyeons-MacBook-Air.local>
2024-03-31 17:05:45 +0900

    fix link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,41 @@ import { Helmet } from "react-helmet-async";
 function App() {
   return (
     <>
-      <Helmet>
+      <Helmet
+        link={[
+          { rel: "icon", href: "./assets/favicon.ico", type: "image/x-icon" },
+          {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "./assets/apple-touch-icon.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "192x192",
+            href: "./assets/android-chrome-192x192.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "512x512",
+            href: "./assets/android-chrome-512x512.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "./assets/favicon-32x32.png",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "./assets/favicon-16x16.png",
+          },
+          { rel: "manifest", href: "./assets/site.webmanifest" },
+        ]}
+      >
         <title>Seo Test...</title>
         <meta property="og:site_name" content="SEO Test" />
         <meta property="og:title" content="SEO Test Page" />
@@ -19,38 +53,6 @@ function App() {
         <meta name="twitter:description" content="SEO Test 입니다" />
         <meta name="twitter:image" content="" />
         <meta name="twitter:card" content="" />
-
-        <link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="./assets/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="./assets/android-chrome-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href="./assets/android-chrome-512x512.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="./assets/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="./assets/favicon-16x16.png"
-        />
-        <link rel="manifest" href="./assets/site.webmanifest" />
       </Helmet>
     </>
   );
